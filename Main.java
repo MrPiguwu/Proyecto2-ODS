de8f6002da3962a224ad91785570f2b34b38ddb0
MrPiguwu <141430510+MrPiguwu@users.noreply.github.com>
2023-10-24 22:21:20 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,33 +1,88 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-// Clase Main
 public class Main {
     public static final int SALIR = 0;
 
-
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Zona> zonas = new ArrayList<>();
 
-        // Menu de opciones
+        int opcion = -1;
 
-        int opcion = 0;
-        while(opcion != SALIR) {
+        while (opcion != SALIR) {
+            System.out.println("Menú de opciones:");
+            System.out.println("1. Crear zona");
+            System.out.println("2. Asignar presupuesto");
+            System.out.println("3. Consultar presupuesto disponible");
+            System.out.println("4. Simular mes");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
 
-            // switch para crear, asignar, consultar, etc.
+            switch (opcion) {
+                case 1:
+                    // Crear zona
+                    System.out.print("Ingrese el nombre de la zona: ");
+                    String nombreZona = scanner.next();
+                    Zona nuevaZona = new Zona(nombreZona);
+                    zonas.add(nuevaZona);
+                    System.out.println("Zona creada con éxito.");
+                    break;
 
-            if(opcion == 1) {
-                // crear zona
-            } else if(opcion == 2) {
-                // asignar presupuesto
-            } else if (opcion == 3) {
-                // consultar disponible
-            } else if(opcion == 4) {
-                // simular mes
+                case 2:
+                    // Asignar presupuesto
+                    System.out.print("Ingrese el nombre de la zona a la que asignar presupuesto: ");
+                    String nombreZonaAsignar = scanner.next();
+                    Zona zonaAsignar = buscarZona(zonas, nombreZonaAsignar);
+
+                    if (zonaAsignar != null) {
+                        System.out.print("Ingrese el monto a asignar: ");
+                        double monto = scanner.nextDouble();
+                        zonaAsignar.asignarPresupuesto(monto);
+                        System.out.println("Presupuesto asignado con éxito.");
+                    } else {
+                        System.out.println("Zona no encontrada.");
+                    }
+                    break;
+
+                case 3:
+                    // Consultar presupuesto disponible
+                    System.out.print("Ingrese el nombre de la zona a consultar: ");
+                    String nombreZonaConsultar = scanner.next();
+                    Zona zonaConsultar = buscarZona(zonas, nombreZonaConsultar);
+
+                    if (zonaConsultar != null) {
+                        System.out.println("Presupuesto disponible en " + zonaConsultar.getNombre() + ": $" + zonaConsultar.consultarDisponible());
+                    } else {
+                        System.out.println("Zona no encontrada.");
+                    }
+                    break;
+
+                case 4:
+                    // Simular mes
+                    for (Zona zona : zonas) {
+                        zona.simularMes();
+                    }
+                    System.out.println("Se ha simulado un mes de gastos.");
+                    break;
+
+                case SALIR:
+                    System.out.println("Saliendo del programa.");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
             }
-
         }
-
     }
 
+    private static Zona buscarZona(ArrayList<Zona> zonas, String nombre) {
+        for (Zona zona : zonas) {
+            if (zona.getNombre().equals(nombre)) {
+                return zona;
+            }
+        }
+        return null;
+    }
 }
